4b528b10743cd8ee7b1f85432ac4933866014e83
Davis <daviscvb22092004@gmail.com>
2024-02-20 07:24:06 -0500

    Creación de Aplicación de SpringBoot

---
+++ src/test/java/com/grupo01/DataStructuresProject/DataStructuresProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo01.DataStructuresProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructuresProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
